02a6956c11aea65bd5383780b5c97427de208ce1
yanghaowei <yanghaowei@tuscloud.io>
2018-02-06 17:14:23 +0800

    new

--- OpenCVProjectswithPy/code/cameo/trackers.py
+++ OpenCVProjectswithPy/code/cameo/trackers.py
@@ -1,1 +1,15 @@
 import cv2 
+import rects
+import utils
+
+class Face(object):
+
+  """Data on facial features: face, eyes, nose, mouth."""
+    
+    def __init__(self):
+        self.faceRect = None
+        self.leftEyeRect = None
+        self.rightEyeRect = None
+        self.noseRect = None
+        self.mouthRect = None
+        
